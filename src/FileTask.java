4ed086e982a3e4ff3ac64deb905ae6f5ffaf603e
Ivan Markovski <sagitariusim@gmail.com>
2022-03-31 20:43:00 +0200

    For grading

---
+++ src/FileTask.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class FileTask
+{
+    public static void main(String[] args)
+    {
+        int countURL = 0;
+        int countIPv6 = 0;
+
+        try
+        {
+            String s;
+            URL url = new URL("https://www.ietf.org/rfc/rfc2732.txt");
+            BufferedReader read = new BufferedReader(new InputStreamReader(url.openStream()));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("src/result.txt", true));
+
+            String[] words= null;
+            while ((s = read.readLine()) != null)
+            {
+                words = s.split("\\s+");
+                for (String word : words)
+                {
+                    if (word.equals("URL") || word.equals("URL's"))
+                    {
+                        countURL++;
+                    }
+                    if (word.equals("Ipv6"));
+                    {
+                        countIPv6++;
+                    }
+                }
+            }
+
+            writer.write("Total URL and URL's words: " + countURL + "\n");
+            writer.write("Total IPv6 words: " + countIPv6 + "\n");
+
+            System.out.println(countURL);
+            System.out.println(countIPv6);
+
+            read.close();
+            writer.close();
+
+        }
+        catch (MalformedURLException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+}
